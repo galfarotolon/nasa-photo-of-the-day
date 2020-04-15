6220c6de68779f167097b3b193b7ffd96c9015de
Guillermo Alfaro <galfarotolon@gmail.com>
2020-04-15 17:05:37 -0500

    mvp completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
+
+const url = "https://api.nasa.gov/planetary/apod"
+const apiKey = "5QLiZdjGEJNCTMappVfsVecGhN1L5gggWZ4tnrSu"
 
 function App() {
+
+  const [data, setGetData] = useState([])
+
+  useEffect(() => {
+    // we put our arbitrary code
+    //we weill fetch with axios
+    //and set our friends in state (happy path)
+
+
+    axios.get(`${url}?api_key=${apiKey}`)
+
+      .then(res => {
+        console.log(res.data)
+        setGetData(res.data) //its an array of freinds
+      })
+      .catch(err => {
+        console.log("not fetching any info")
+
+      })
+
+  }, [])
+
+
+  const img = data.hdurl
+
+  console.log(img)
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+
+
+
+      <h1>{data.title}</h1>
+      <div className="date">Image Date: {data.date}</div>
+      <div><img src={img} /></div>
+      <div className="copyright">Photo by {data.copyright}. All Rights Reserved.</div>
+
+      <div className="explanation">{data.explanation}</div>
+
     </div>
   );
 }
 
 export default App;
+
